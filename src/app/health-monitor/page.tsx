22ecac6366354efc7828995056aaeedea7358d9e
HappyRehund <rayhanardian045@gmail.com>
2025-05-16 19:37:24 +0700

    try fix 405 #3

--- src/app/health-monitor/page.tsx
+++ src/app/health-monitor/page.tsx
@@ -2,6 +2,7 @@ import MonitoringData from '@/components/module/health-monitor/MonitoringData'
 import MonitoringGraph from '@/components/module/health-monitor/MonitoringGraph'
 import React from 'react'
 
+export const runtime = 'edge'
 function HealthMonitorPage() {
   return (
     <>
